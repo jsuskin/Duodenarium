0f065d8a8de3a1843bc0f5421ebdbff20ac490ff
jsuskin <jsuskin85@gmail.com>
2019-12-16 00:27:21 -0500

    update welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     songs = Song.all
-    render json: songs
+    render json: songs.to_json( :only => [:id, :title, :url, :image, :created_at], :include => [{ :artist => { :only => :name } }, { :tags => { :only => :name } }, { :external_links_list => { :only => [:discogs, :bandcamp, :soundcloud] } }] )
   end
 end
