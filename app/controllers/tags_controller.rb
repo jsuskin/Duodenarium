eb335b8c0ada39eb2d4ded27b69e43ec4b8c2b9d
jsuskin <jsuskin85@gmail.com>
2019-12-16 03:49:41 -0500

    add update action to Tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,6 +8,16 @@ class TagsController < ApplicationController
     end
   end
 
+  def update
+    @tag = Tag.find(params[:id])
+    @tag.update(tag_params)
+    if @tag.save
+      render json: @tag
+    else
+      render json: { errors: tag.errors.full_messages }, status: 422
+    end
+  end
+
   private
 
   def tag_params
